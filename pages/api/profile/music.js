5a38d0a52dae915822999aa91003ddc9a753d992
eldossjogy <eldossjogy@gmail.com>
2022-07-19 11:40:28 -0400

    Fixed Music Fetch Data

--- pages/api/profile/music.js
+++ pages/api/profile/music.js
@@ -4,15 +4,32 @@ export default async function musicInfo(req, res) {
         let endPos = body.search(endSearch);
         let result = body.substring((startPos + startSearch.length), endPos);
         result = result.trim()
-        return result.substring(1, result.length - 1)
+        return result.substring(2, result.length - 1)
+    }
+
+    function CurrentListeningArtist(body, startSearch, endSearch) {
+        let newBody = body.replace(startSearch, "");
+        let startPos = newBody.search(startSearch);
+        newBody = newBody.substring(startPos, newBody.length)
+        startPos = newBody.search(startSearch);
+        let endPos = newBody.search(endSearch);
+        let result = newBody.substring((startPos + startSearch.length + 2), endPos);
+        let endLocal = result.search('"');
+        result = result.substring(0,endLocal);
+        result = result.trim()
+        return result
     }
 
     async function fetchMusic() {
         let Music = {}
         const response = await fetch('https://www.last.fm/user/Betamxnster');
-        const data = await response.text();
-        let track = CurrentListening(data, "data-track-name=", "data-track-url")
-        let artist = CurrentListening(data, "data-artist-name=", "data-artist-url=")
+        var data = await response.text();
+        var chartListpos = data.search("chartlist-row--now-scrobbling")
+        data = data.substring(chartListpos, data.length);
+        var chartListpos = data.search("chartlist-name")
+        data = data.substring(chartListpos, data.length);
+        let track = CurrentListening(data, "title", "class")
+        let artist = CurrentListeningArtist(data, "title", "</a>")
         track = track.replace("&#39;", "'").replace("&amp;", "&").replace("&#34;", `"`).replace("&#34;", `"`)
         artist = artist.replace("&#39;", "'").replace("&amp;", "&").replace("&#34;", `"`).replace("&#34;", `"`)
         Music.track = decodeURI(track);
