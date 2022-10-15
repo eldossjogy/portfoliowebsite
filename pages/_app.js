a3982f9d5cefc0f3286d000e7970c30d08e2e991
eldossjogy <eldossjogy@gmail.com>
2022-10-15 03:26:07 -0400

    fixe maybe

--- pages/_app.js
+++ pages/_app.js
@@ -17,11 +17,6 @@ function MyApp({ Component, pageProps }) {
           <Head>
             <title>Eldoss Jogy</title>
             <link rel="icon" type="image/x-png" href="./icon.png"></link>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta property="og:title" content="Hi, I'm Eldoss Jogy." />
-            <meta property="og:description" content="Eldoss Jogy's Portfolio Website" />
-            <meta property="og:url" content="https://eldossjogy.vercel.app/" />
-            <meta property="og:image" content="https://avatars.githubusercontent.com/u/77898541?v=4" />
           </Head>
           <AppWrapper >
             <Component {...pageProps} />
