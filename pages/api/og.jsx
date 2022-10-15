8c62fae042cf1a9a6d19bc77b3a27fbf46ad01be
eldossjogy <eldossjogy@gmail.com>
2022-10-15 15:25:44 -0400

    added github image

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -82,10 +82,10 @@ export default function handler(req) {
                 }}
             >
                 <div style={{ display: 'flex', height: '40%', alignItems: 'center', paddingTop: '30px' }}>
-                    {/* <img src="https://avatars.githubusercontent.com/u/77898541?s=400&u=30cc411c43ed339f9136b0e6b59206aef5822fb0&v=4" alt="pfp" width="250" height="250" style={{
+                    <img src="https://avatars.githubusercontent.com/u/77898541?s=400&u=30cc411c43ed339f9136b0e6b59206aef5822fb0&v=4" alt="pfp" width="250" height="250" style={{
                         marginLeft: 20,
                         borderRadius: '50px',
-                    }} /> */}
+                    }} />
 
                     <div style={{
                         display: 'flex',
